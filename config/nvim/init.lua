c0c1e1e536ce5233fcdd88f8515cbd5e3a93546f
bethanyj28 <bethanyjanos28@gmail.com>
2023-02-23 22:00:44 -0500

    maybe change order?

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -415,43 +415,6 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- Go goodies
-require('go').setup({
-    -- notify: use nvim-notify
-    notify = false,
-    -- auto commands
-    auto_format = true,
-    auto_lint = true,
-    -- linters: revive, errcheck, staticcheck, golangci-lint
-    linter = 'revive',
-    -- linter_flags: e.g., {revive = {'-config', '/path/to/config.yml'}}
-    linter_flags = {},
-    -- lint_prompt_style: qf (quickfix), vt (virtual text)
-    lint_prompt_style = 'qf',
-    -- formatter: goimports, gofmt, gofumpt, lsp
-    formatter = 'goimports',
-    -- maintain cursor position after formatting loaded buffer
-    maintain_cursor_pos = false,
-    -- test flags: -count=1 will disable cache
-    test_flags = {'-v'},
-    test_timeout = '30s',
-    test_env = {},
-    -- show test result with popup window
-    test_popup = true,
-    test_popup_auto_leave = false,
-    test_popup_width = 80,
-    test_popup_height = 10,
-    -- test open
-    test_open_cmd = 'edit',
-    -- struct tags
-    tags_name = 'json',
-    tags_options = {'json=omitempty'},
-    tags_transform = 'snakecase',
-    tags_flags = {'-skip-unexported'},
-    -- quick type
-    quick_type_flags = {'--just-types'},
-})
-
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -581,6 +544,43 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- Go goodies
+require('go').setup({
+    -- notify: use nvim-notify
+    notify = false,
+    -- auto commands
+    auto_format = true,
+    auto_lint = true,
+    -- linters: revive, errcheck, staticcheck, golangci-lint
+    linter = 'revive',
+    -- linter_flags: e.g., {revive = {'-config', '/path/to/config.yml'}}
+    linter_flags = {},
+    -- lint_prompt_style: qf (quickfix), vt (virtual text)
+    lint_prompt_style = 'qf',
+    -- formatter: goimports, gofmt, gofumpt, lsp
+    formatter = 'goimports',
+    -- maintain cursor position after formatting loaded buffer
+    maintain_cursor_pos = false,
+    -- test flags: -count=1 will disable cache
+    test_flags = {'-v'},
+    test_timeout = '30s',
+    test_env = {},
+    -- show test result with popup window
+    test_popup = true,
+    test_popup_auto_leave = false,
+    test_popup_width = 80,
+    test_popup_height = 10,
+    -- test open
+    test_open_cmd = 'edit',
+    -- struct tags
+    tags_name = 'json',
+    tags_options = {'json=omitempty'},
+    tags_transform = 'snakecase',
+    tags_flags = {'-skip-unexported'},
+    -- quick type
+    quick_type_flags = {'--just-types'},
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
